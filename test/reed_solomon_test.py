662ebbd9294b0b342d10c625e0e9dfd53b935f11
Kirill Smirnov <smirnowku@gmail.com>
2019-11-11 22:19:52 +0300

    Add Reed-Solomon encoding test

--- test/reed_solomon_test.py
+++ test/reed_solomon_test.py
@@ -1,2 +1,17 @@
-def test():
-    assert 1 == 1
+import pytest
+import random
+from main.reed_solomon import encoding_rs, decoding_rs
+
+k = 12  # input message length
+
+
+def test_encoding():
+    for attempt in range(100):
+        input_message = []
+        for i in range(k):
+            input_message.append(random.randint(0, 1))
+
+        try:
+            encoding_rs(input_message)
+        except:
+            pytest.fail()
