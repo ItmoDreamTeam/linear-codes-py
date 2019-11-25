c94558a2e0d968aaa8f24b95baed97d29e16ffe0
Kirill Smirnov <smirnowku@gmail.com>
2019-11-25 19:22:07 +0300

    Improve Reed-Solomon (#7)

---
+++ main/utils.py
@@ -0,0 +1,14 @@
+from typing import *
+
+
+def stringToList(message: str) -> List[int]:
+    return list(map(lambda bit: int(bit), message))
+
+
+def listToString(message: List[int], size: int) -> str:
+    representation = "".join(map(lambda bit: str(int(bit)), message))
+    return "0" * (size - len(representation)) + representation
+
+
+def normalize(polynomial) -> List[int]:
+    return list(map(lambda e: abs(int(e)) % 2, polynomial))
