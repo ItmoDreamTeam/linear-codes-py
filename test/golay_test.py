a52e51dcf672ace533d2dfe072e9e5d8b047a58c
Kirill Smirnov <smirnowku@gmail.com>
2019-11-26 17:39:17 +0300

    Add tests for double and triple error Golay code

--- test/golay_test.py
+++ test/golay_test.py
@@ -20,3 +20,29 @@ def test_with_single_error():
         corrupted_bit = '0' if encoded_codeword[error_index] == '1' else '1'
         corrupted = encoded_codeword[:error_index] + corrupted_bit + encoded_codeword[error_index + 1:]
         assert decode(corrupted) == source
+
+
+def test_with_two_errors():
+    source = "111010111001"
+    encoded_codeword = encode(source)
+    for _ in range(1000):
+        corrupted = make_errors(encoded_codeword, 2)
+        assert decode(corrupted) == source
+
+
+def test_with_three_errors():
+    source = "111010111001"
+    encoded_codeword = encode(source)
+    for _ in range(1000):
+        corrupted = make_errors(encoded_codeword, 3)
+        assert decode(corrupted) == source
+
+
+def make_errors(codeword: str, errors_count: int) -> str:
+    errors_indices = set()
+    while len(errors_indices) < errors_count:
+        errors_indices.add(random.randint(0, len(codeword) - 1))
+    for error_index in errors_indices:
+        corrupted_bit = '0' if codeword[error_index] == '1' else '1'
+        codeword = codeword[:error_index] + corrupted_bit + codeword[error_index + 1:]
+    return codeword
